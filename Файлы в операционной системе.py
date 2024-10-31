2b9cfaf7796a308912347dc3b5275f59642dc853
Whale-99 <168347567+Whale-99@users.noreply.github.com>
2024-10-31 10:47:36 +0300

    Add files via upload

---
+++ Файлы в операционной системе.py
@@ -0,0 +1,25 @@
+import os
+import time
+
+# Указываем каталог для обхода, например, текущий каталог
+directory = "."
+
+# Обход каталога с помощью os.walk
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        # Формируем полный путь к файлу
+        filepath = os.path.join(root, file)
+
+        # Получаем время последнего изменения файла
+        filetime = os.path.getmtime(filepath)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+
+        # Получаем размер файла
+        filesize = os.path.getsize(filepath)
+
+        # Получаем родительскую директорию файла
+        parent_dir = os.path.dirname(filepath)
+
+        # Выводим информацию о файле
+        print(
+            f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
